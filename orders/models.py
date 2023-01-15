65590c835b721fdd9233f9a74ec911a3de43a876
Yousef <yousefghazal2412@gmail.com>
2023-01-15 17:17:32 +0200

    stripe url

--- orders/models.py
+++ orders/models.py
@@ -24,6 +24,18 @@ class Order(models.Model):
     def get_total_cost(self):
         return sum(item.get_cost() for item in self.items.all())
 
+    def get_stripe_url(self):
+        if not self.stripe_id:
+            # no payment associated
+            return ''
+        if '_test_' in settings.STRIPE_SECRET_KEY:
+            # Stripe path for test payments
+            path = '/test/'
+        else:
+            # Stripe path for real payments
+            path = '/'
+        return f'https://dashboard.stripe.com{path}payments/{self.stripe_id}'
+
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
     product = models.ForeignKey(Product, related_name='order_items', on_delete=models.CASCADE)
--- payment/webhooks.py
+++ payment/webhooks.py
@@ -3,7 +3,6 @@ from django.conf import settings
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from orders.models import Order
-from payment.views import payment_completed
 
 @csrf_exempt
 def stripe_webhook(request):
